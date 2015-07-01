9ab7ddfdc80b16f5a67c2c2d5b83491cc8cb97a7
bosaichen <bchen04@syr.edu>
2015-06-30 23:50:10 -0700

    add arcprogress

--- app/src/main/java/cooluiux/demo/com/uiuxdemo/MainActivity.java
+++ app/src/main/java/cooluiux/demo/com/uiuxdemo/MainActivity.java
@@ -1,17 +1,62 @@
 package cooluiux.demo.com.uiuxdemo;
 
+import android.animation.ValueAnimator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import cooluiux.demo.com.uiuxdemo.widget.ArcProgress;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    ArcProgress mArcProgress;
+    Timer mTimer;
+    ValueAnimator mAnimator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mArcProgress = (ArcProgress) findViewById(R.id.arc_progress);
+
+        //use Timer
+        /*mTimer = new Timer();
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mArcProgress.setProgress(mArcProgress.getProgress() + 1);
+                    }
+                });
+            }
+        }, 1000, 100);*/
+
+        //use ValueAnimator
+        ValueAnimator animator = ValueAnimator.ofInt(0, 100);
+
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                int currentValue = (int)valueAnimator.getAnimatedValue();
+                mArcProgress.setProgress(currentValue);
+            }
+        });
+
+        animator.setTarget(mArcProgress);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setDuration(5000);
+        animator.setRepeatCount(3);
+        animator.setRepeatMode(ValueAnimator.RESTART);
+        animator.start();
     }
 
 
@@ -35,5 +80,12 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        mTimer.cancel();
+        mAnimator.cancel();
     }
 }
